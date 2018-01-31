2610cad775baef0175a54b11842c4ffb0c0835ec
Michael Bestas <mkbestas@lineageos.org>
2018-01-31 22:41:21 +0300

    LineageParts: Restore keydisabler state at boot
    Change-Id: Id8ad037ede52c801173cccbf13288120f1475578

--- src/org/lineageos/lineageparts/BootReceiver.java
+++ src/org/lineageos/lineageparts/BootReceiver.java
@@ -25,10 +25,9 @@ import android.support.v7.preference.PreferenceManager;
 /*
 import org.lineageos.lineageparts.contributors.ContributorsCloudFragment;
 import org.lineageos.lineageparts.gestures.TouchscreenGestureSettings;
-import org.lineageos.lineageparts.input.ButtonSettings;
 */
+import org.lineageos.lineageparts.input.ButtonSettings;
 
-/*
 public class BootReceiver extends BroadcastReceiver {
 
     private static final String TAG = "BootReceiver";
@@ -38,15 +37,16 @@ public class BootReceiver extends BroadcastReceiver {
     public void onReceive(Context ctx, Intent intent) {
         if (!hasRestoredTunable(ctx)) {
             /* Restore the hardware tunable values */
-            /*
             ButtonSettings.restoreKeyDisabler(ctx);
             setRestoredTunable(ctx);
         }
 
+        /*
         TouchscreenGestureSettings.restoreTouchscreenGestureStates(ctx);
 
         // Extract the contributors database
         ContributorsCloudFragment.extractContributorsCloudDatabase(ctx);
+        */
     }
 
     private boolean hasRestoredTunable(Context context) {
@@ -59,4 +59,3 @@ public class BootReceiver extends BroadcastReceiver {
         preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
     }
 }
-*/
