ee907db2e765ecf0b402d03530e06f0399388662
mosimchah <mosimchah@gmail.com>
2018-02-22 07:30:22 -0500

    Revert "PowerMenu: add Screenrecord to the menu [3/3]"
    This reverts commit c6f3c80952b7b31988d42600d1456e09471431d9.
    
    Change-Id: I36c24397d0831ce8980d6a4fb6e4dc2bd8a34536

--- src/org/lineageos/lineageparts/input/PowerMenuActions.java
+++ src/org/lineageos/lineageparts/input/PowerMenuActions.java
@@ -42,7 +42,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
     final static String TAG = "PowerMenuActions";
 
     private CheckBoxPreference mScreenshotPref;
-    private CheckBoxPreference mScreenrecordPref;
     private CheckBoxPreference mAirplanePref;
     private CheckBoxPreference mUsersPref;
     private CheckBoxPreference mBugReportPref;
@@ -63,8 +62,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         for (String action : mAllActions) {
             if (action.equals(GLOBAL_ACTION_KEY_SCREENSHOT)) {
                 mScreenshotPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_SCREENSHOT);
-            } else if (action.equals(GLOBAL_ACTION_KEY_SCREENRECORD)) {
-                mScreenrecordPref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_SCREENRECORD);
             } else if (action.equals(GLOBAL_ACTION_KEY_AIRPLANE)) {
                 mAirplanePref = (CheckBoxPreference) findPreference(GLOBAL_ACTION_KEY_AIRPLANE);
             } else if (action.equals(GLOBAL_ACTION_KEY_USERS)) {
@@ -83,10 +80,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
 
         if (mScreenshotPref != null) {
             mScreenshotPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SCREENSHOT));
-        }
-
-        if (mScreenrecordPref != null) {
-            mScreenrecordPref.setChecked(settingsArrayContains(GLOBAL_ACTION_KEY_SCREENRECORD));
         }
 
         if (mAirplanePref != null) {
@@ -126,10 +119,6 @@ public class PowerMenuActions extends SettingsPreferenceFragment {
         if (preference == mScreenshotPref) {
             value = mScreenshotPref.isChecked();
             updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENSHOT);
-
-        } else if (preference == mScreenrecordPref) {
-            value = mScreenrecordPref.isChecked();
-            updateUserConfig(value, GLOBAL_ACTION_KEY_SCREENRECORD);
 
         } else if (preference == mAirplanePref) {
             value = mAirplanePref.isChecked();
