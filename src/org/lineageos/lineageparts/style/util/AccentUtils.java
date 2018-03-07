2bd3a1ad993ff24fe3eb66e9eaa666614bea4234
mosimchah <mosimchah@gmail.com>
2018-03-07 14:21:28 -0500

    LineageParts: Update the color accent to pixel blue
    Change-Id: Id5d097a41e702d8a56760ef4eef8929a66419752

--- src/org/lineageos/lineageparts/style/util/AccentUtils.java
+++ src/org/lineageos/lineageparts/style/util/AccentUtils.java
@@ -94,7 +94,7 @@ public final class AccentUtils {
     @NonNull
     private static Accent getDefaultAccent(Context context) {
         return new Accent(context.getString(R.string.style_accent_default_name),
-                StyleInterface.ACCENT_DEFAULT, Color.parseColor("#4182EF"), StyleStatus.DYNAMIC);
+                StyleInterface.ACCENT_DEFAULT, Color.parseColor("#3367D6"), StyleStatus.DYNAMIC);
     }
 
     public static boolean isCompatible(StyleStatus currentStatus, Accent accent) {
